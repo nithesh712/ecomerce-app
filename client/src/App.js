e968131ea67fdfb66580f5a520c7b4630c2fc461
nithesh <nithesh.ch712@gmail.com>
2021-01-30 19:51:21 +0530

    React Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Shop App</h1>
-    </div>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route exact path="/" component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 

fcab330a81efdd1d918586b80cfa9479fd5133a3
nithesh <nithesh.ch712@gmail.com>
2021-02-09 17:43:13 +0530

    search functionality

--- client/src/App.js
+++ client/src/App.js
@@ -39,6 +39,7 @@ function App() {
           <Route path="/admin/productlist" component={ProductListScreen} />
           <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
+          <Route path="/search/:keyword" component={HomeScreen} />
           <Route exact path="/" component={HomeScreen} />
         </Container>
       </main>
